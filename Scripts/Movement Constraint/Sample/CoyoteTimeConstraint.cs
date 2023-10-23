53fb5df63a9cc0afce361e41eb58e51f994e752d
ChichoRD <chichord2005@hotmail.com>
2023-10-23 22:57:37 +0200

    Jump Buffer and Coyote Time and Fall Negation

---
+++ Scripts/Movement Constraint/Sample/CoyoteTimeConstraint.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoyoteTimeConstraint : MonoBehaviour, IMovementConstraint
+{
+    [RequireInterface(typeof(IMovementConstraint))]
+    [SerializeField]
+    private Object _movementConstraintObject;
+    private IMovementConstraint MovementConstraint => _movementConstraintObject as IMovementConstraint;
+
+    [RequireInterface(typeof(IObservableJumpController))]
+    [SerializeField]
+    private Object _observableJumpControllerObject;
+    private IObservableJumpController ObservableJumpController => _observableJumpControllerObject as IObservableJumpController;
+
+    [RequireInterface(typeof(IDurationProvider))]
+    [SerializeField]
+    private Object _durationProviderObject;
+    private IDurationProvider DurationProvider => _durationProviderObject as IDurationProvider;
+
+    private Coroutine _coyoteTimeCoroutine;
+
+    private void Awake()
+    {
+        ObservableJumpController.DescentStarted.AddListener(OnDescentStarted);
+    }
+
+    private void OnDestroy()
+    {
+        ObservableJumpController.DescentStarted.RemoveListener(OnDescentStarted);
+    }
+
+    public bool CanPerformMovement() => MovementConstraint.CanPerformMovement() || TryStopCoyoteTime();
+
+    private bool TryStartCoyoteTime()
+    {
+        if (_coyoteTimeCoroutine != null) return false;
+        _coyoteTimeCoroutine = StartCoroutine(CoyoteTimeCoroutine());
+        return true;
+    }
+
+    private bool TryStopCoyoteTime()
+    {
+        if (_coyoteTimeCoroutine == null) return false;
+        StopCoroutine(_coyoteTimeCoroutine);
+        _coyoteTimeCoroutine = null;
+        return true;
+    }
+
+    private IEnumerator CoyoteTimeCoroutine()
+    {
+        yield return new WaitForSeconds((float)(DurationProvider?.GetDuration().TotalSeconds ?? 0.0f));
+        _coyoteTimeCoroutine = null;
+    }
+
+    private void OnDescentStarted()
+    {
+        _ = MovementConstraint.CanPerformMovement()
+            && (TryStartCoyoteTime()
+                || (TryStopCoyoteTime()
+                    && TryStartCoyoteTime()));
+    }
+}
---
+++ Scripts/Movement Performer/Sample/BufferedJumpImpulsePerformer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BufferedJumpImpulsePerformer : MonoBehaviour, IMovementPerformer
+{
+    [RequireInterface(typeof(IMovementPerformer))]
+    [SerializeField]
+    private Object _movementPerformerObject;
+    private IMovementPerformer MovementPerformer => _movementPerformerObject as IMovementPerformer;
+
+    [RequireInterface(typeof(IObservableJumpController))]
+    [SerializeField]
+    private Object _observableJumpControllerObject;
+    private IObservableJumpController ObservableJumpController => _observableJumpControllerObject as IObservableJumpController;
+
+    [RequireInterface(typeof(IDurationProvider))]
+    [SerializeField]
+    private Object _durationProviderObject;
+    private IDurationProvider DurationProvider => _durationProviderObject as IDurationProvider;
+
+    private Coroutine _performedLastMovementCoroutine;
+    private bool _performedLastMovement;
+
+    private void Awake()
+    {
+        ObservableJumpController.DescentEnded.AddListener(OnDescentEnded);
+    }
+
+    private void OnDestroy()
+    {
+        ObservableJumpController.DescentEnded.RemoveListener(OnDescentEnded);
+    }
+
+    public bool TryPerformMovement() => (_performedLastMovement = MovementPerformer.TryPerformMovement())
+                                        & (TryStartPerformedLastMovementCoroutine()
+                                           || (TryStopPerformedLastMovementCoroutine()
+                                               && TryStartPerformedLastMovementCoroutine()));
+
+    private void OnDescentEnded()
+    {
+        _ = _performedLastMovement
+            || !TryStopPerformedLastMovementCoroutine()
+            || TryPerformMovement();
+    }
+
+    private bool TryStartPerformedLastMovementCoroutine()
+    {
+        if (_performedLastMovementCoroutine != null) return false;
+        _performedLastMovementCoroutine = StartCoroutine(PerformedLastMovementCoroutine());
+        return true;
+    }
+
+    private bool TryStopPerformedLastMovementCoroutine()
+    {
+        if (_performedLastMovementCoroutine == null) return false;
+        StopCoroutine(_performedLastMovementCoroutine);
+        _performedLastMovementCoroutine = null;
+        return true;
+    }
+
+    private IEnumerator PerformedLastMovementCoroutine()
+    {
+        yield return new WaitForSeconds((float)(DurationProvider?.GetDuration().TotalSeconds ?? 0.0f));
+        _performedLastMovementCoroutine = null;
+    }
+}
